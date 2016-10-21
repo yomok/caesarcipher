7982d03d3b56985eda8bf04b35bbaffede4efac9
len lee <lenlee454@gmail.com>
2016-10-20 21:29:06 -0700

    stocksman

--- stocks.rb
+++ stocks.rb
@@ -1,20 +1,20 @@
-
 def stock_picker(prices)
-	best_days = []
-    sell_day = 0
-    buy_day = 0
-    difference = 0
-    	
-    prices.each do |price|
-    	prices.each do |day|
-    		if day - price > difference && prices.index(day) > prices.index(price)
-    			difference = day - price
-    			sell_day = prices.index(day)
-    			buy_day = prices.index(price)
-    		end
-    	end
-	end
-    		best_days = [buy_day, sell_day]
+  max_money = 0
+  buy_day = 0
+  sell_day = 0
+  (0..(prices.length-1)).each do |day|
+    sell = day + 1
+    (sell..(prices.length-1)).each do |day2|
+      diff = prices[day2] - prices[day]
+      if diff > max_money
+        max_money = diff
+        buy_day = day
+        sell_day = day2
+      end
+    end
+  end
+  "buy on day #{buy_day}, sell on day #{sell_day} for a profit of #{max_money}"
 end
+
 
 puts stock_picker([17,3,6,9,15,8,6,1,10])
