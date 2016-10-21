b71b39a89831db1f00f2f1e41128d227fdd5c532
len lee <lenlee454@gmail.com>
2016-10-20 21:59:57 -0700

    single word

--- substring.rb
+++ substring.rb
@@ -1,5 +1,13 @@
 dictionary =["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
 
-def substrings(word, dic)
-  
+def substrings(str, dic)
+  new_hash = {}
+  dic.each do |word|
+    if word == str
+      new_hash[str] = new_hash.fetch(word, 0) + 1
+    end
+  end
+  new_hash
 end
+
+puts substrings("below", dictionary)
