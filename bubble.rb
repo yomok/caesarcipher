f6bb2ae97f243ae52f8f7ea262d4284cd4bfc0e5
len lee <lenlee454@gmail.com>
2016-10-24 14:22:38 -0700

    sort_by block

--- bubble.rb
+++ bubble.rb
@@ -1,3 +1,4 @@
+
 def bubble_sort(array)
   counter = 0
   x = true
@@ -20,3 +21,29 @@ end
 end
 puts bubble_sort([4,3,78,2,0,2])
 puts bubble_sort([5,100,2, 25, 1, 10, -7, 4, 200, 75])
+
+
+
+def bubble_sort_by(array)
+  arr_length = array.length
+  
+  arr_length.times do
+    count = 0
+    for i in 0..(arr_length - 2)
+      if yield(array[i], array[i+1]) > 0
+        array[i], array[i+1] = array[i+1], array[i]
+        count += 1
+      end
+    end
+    if count != 0
+      count = 0
+    else
+      break
+    end
+  end
+  array
+end
+
+bubble_sort_by(["hi","hello","hey"]) do |left,right|
+  left.length - right.length
+end
